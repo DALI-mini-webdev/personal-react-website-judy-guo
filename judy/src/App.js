a900c777495a822a9ff5ecefce60ee98f0079d3d
judy-guo <judy.guo.24@dartmouth.edu>
2021-02-13 23:38:04 -0500

    add brain food

--- judy/src/App.js
+++ judy/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar/Navbar';
 import Intro from './components/Intro'
 import Work from './components/Work'
 import Play from './components/Play'
+import axios from 'axios'; 
+
 
 class App extends Component {
   constructor(props) {
@@ -29,7 +31,38 @@ class App extends Component {
     }
   }
  
-  
+  fetchData = () => {
+    // It will take an indeterminate amount of time for axios to get the data
+    // so we use this then/catch format
+    // this says: hit this endpoint. Then when you're done do X. 
+    // If there is an error at any point, do Y
+     axios.get("https://poetrydb.org/random")
+     .then((response) => {
+       console.log(response);
+       this.setState({data: response.data[0]})
+     }).catch((error) => {
+       console.log(error)
+     })
+
+ };
+
+ renderData = () => {
+   if(this.state.data) {
+     return(
+       <div>
+         <div>{this.state.data.title}</div>
+         <div>By {this.state.data.author}</div>
+         {this.state.data.lines.map((line, idx) => {
+           return(<div key ={idx}>{line}</div>)
+         })}
+
+       </div>
+     )
+   }else{
+     return null
+   }
+ }
+
   activityList = ["hiking", "canoeing", "procrasti-baking"];
  
   // activityMap = activityList.map((activity) => {
@@ -50,18 +83,17 @@ class App extends Component {
       <div>
       <div className="App">
         <Navbar changeCurrPage = {this.changeCurrPage}/>
-        {this.renderPage()}        
+        {this.renderPage()}  
+        <button onClick={this.fetchData}>click for brain food!</button>
+            {this.renderData()}
+  
        
       </div>
 
-  
     </div>
     );
   }
   
- 
-  
-
 
 }
 export default App;
